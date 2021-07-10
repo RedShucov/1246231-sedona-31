9ecbc95f9d37742d628c4d57294cd0ec238ed9a6
redshucov <redshucov@gmail.com>
2021-07-10 12:40:13 +0300

    caralog пиксель перфект 1/2

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelector('.hotel-search__pop-up').classList.add('js-active');
+
+
+let hidden = document.querySelector('.hotel-search__pop-up');
+let showButton = document.querySelector('.hotel-search_button-search');
+
+showButton.onclick = function() {
+  hidden.classList.toggle('js-active');
+};
