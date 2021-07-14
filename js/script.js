b4a1f61c709ff1844d23806d5823dc4d395a3612
redshucov <redshucov@gmail.com>
2021-07-14 17:34:16 +0300

    Анимация popup

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-document.querySelector('.hotel-search__pop-up').classList.add('js-active');
+const popupShow = document.querySelector(".hotel-search__show-button");
+const popup = document.querySelector(".hotel-search__pop-up");
+const form = popup.querySelector("form")
 
-let hidden = document.querySelector('.hotel-search__pop-up');
-let showButton = document.querySelector('.hotel-search_button-search');
+const buttonSearch = form.querySelector(".pop-up__button");
+const dateStart = form.querySelector("[id=date-start]");
+const dateEnd = form.querySelector("[id=date-end]");
+const countAdult = form.querySelector("[id=count-adults]");
+const countChild = form.querySelector("[id=count-childrens]");
 
-showButton.onclick = function() {
-  hidden.classList.toggle('js-active');
+popup.classList.add("popup-show");
+
+popupShow.onclick = function () {
+  popup.classList.toggle("popup-show");
+
+  dateStart.focus({
+    preventScroll: true
+  });
 };
+
+form.addEventListener("submit", function (evt) {
+  if (!dateStart.value || !dateEnd.value || !countAdult.value || !countChild.value) {
+    evt.preventDefault();
+    popup.classList.remove("popup-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("popup-error");
+  }
+});
